662c4fd4cb3f62977b775516d18290e86ada2a5e
Darachhat SOTHUN <129612348+Darachhat@users.noreply.github.com>
2025-05-28 19:52:00 +0700

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    image: { type: String, required: true },
+    image: { type: String },
     address: { type: Object, default: {line1:"", line2:""} },
     gender: { type: String, default:"Not Selected" },
     dob: { type: String, default:"Not Selected" },
